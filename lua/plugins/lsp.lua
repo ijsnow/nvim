c857d315d286c5ac2033bf826ef46b30d961bb47
Isaac Snow <isaacjsnow@gmail.com>
2023-02-22 20:00:32 -0800

    up

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      autoformat = false,
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.stylua,
+          nls.builtins.diagnostics.flake8,
+          nls.builtins.formatting.black,
+          nls.builtins.formatting.eslint,
+          nls.builtins.formatting.prettier.with({
+            filetypes = {
+              "css",
+              "scss",
+              "less",
+              "html",
+              "json",
+              "jsonc",
+              "yaml",
+              "markdown",
+              "markdown.mdx",
+              "graphql",
+              "javascript",
+            },
+          }),
+        },
+      }
+    end,
+  },
+}
